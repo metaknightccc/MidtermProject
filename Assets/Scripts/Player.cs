9d78820b7664b13bee09bab795c8256c352d4c43
William Yang <wy2037@nyu.edu>
2022-10-08 00:37:04 -0400

    Double Jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,36 @@ public class Player : MonoBehaviour
     public int speed = 5;
     public int jumpForce = 800;
 
+    public Transform feet;
+    public LayerMask groundLayer;
+    public float radius = 0.3f;
+    public bool grounded = false;
+    public int extraJumps = 1;
+
     public Rigidbody2D rb;
     float xSpeed = 0;
 
     // int endLag;
     // int hitStun;
+
+    void Update() {
+        grounded = Physics2D.OverlapCircle(feet.position, radius, groundLayer);
+        if(Input.GetButtonDown("Jump"))
+        {
+            if (grounded) {
+                rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
+            } else {
+                if (extraJumps > 0) {
+                    extraJumps -= 1;
+                    rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
+                }
+            }
+        }
+        if (grounded) {
+            extraJumps = 1;
+        }
+    }
+
     void FixedUpdate()
     {
         xSpeed = Input.GetAxis("Horizontal") * speed;
