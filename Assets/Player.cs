0af9f15db727baa253fb186cc83420c84ca20be2
William Yang <wy2037@nyu.edu>
2022-10-08 00:13:12 -0400

    First changes

---
+++ Assets/Player.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int speed = 5;
+    public int jumpForce = 800;
+
+    public Rigidbody2D rb;
+    float xSpeed = 0;
+
+    // int endLag;
+    // int hitStun;
+    void FixedUpdate()
+    {
+        xSpeed = Input.GetAxis("Horizontal") * speed;
+        rb.velocity = new Vector2(xSpeed, 0f);
+    }
+}
