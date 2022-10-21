28d7914db625144f6b04036925f147d9ace6fb03
metaknightccc <sunyifanc@126.com>
2022-10-20 21:05:45 -0400

    Turn the health system into smash bro style

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 
 public class HealthSystem : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI rateText;
     [SerializeField] private GameObject[] hearts;
     private int life;
     private bool isDead;
+    private float rate=0f;
 
     private void Start()
     {
@@ -21,15 +25,16 @@ public class HealthSystem : MonoBehaviour
         if (isDead == true)
         {
             Debug.Log("Dead");
-            SceneManager.LoadScene("Test");
+            SceneManager.LoadScene("Test"); // change to win page
         }
     }
 
-    public void TakeDamage(int damage)
+    public void LoseOneHeart()
     {
+        rate = 0f;
         if (life >= 1)
         {
-            life -= damage;
+            life -= 1;
             hearts[life].gameObject.SetActive(false);
             if (life < 1)
             {
@@ -37,6 +42,12 @@ public class HealthSystem : MonoBehaviour
             }
             
         }
+    }
+
+    public void Damage(float damage)
+    {
+        rate += damage;
+        rateText.text = rate.ToString()+"%";
     }
     
     public bool Health(int healthValue)
