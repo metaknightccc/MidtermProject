b1d8a8d8d061740e6b44eaaf201a231a3d5fb5cf
William Yang <wy2037@nyu.edu>
2022-10-26 13:00:20 -0400

    Finished Moveset, Testing local multiplayer

---
+++ Assets/Scripts/rotate.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    public float rotationSpeed;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,0,rotationSpeed*Time.deltaTime);
+    }
+}
