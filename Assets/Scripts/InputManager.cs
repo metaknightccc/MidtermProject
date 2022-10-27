d667742a34e60a535ea59536fcb93066bb43c0b0
William Yang <wy2037@nyu.edu>
2022-10-27 02:06:36 -0400

    Reverted to old input system

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -15,15 +15,15 @@ public class InputManager : MonoBehaviour
         Debug.Log(playerCount);
 
         if(playerCount < 1) {
-            PlayerInput.Instantiate(playerPrefabs[0], controlScheme: keyActions[0].name);
-            PlayerInput.Instantiate(playerPrefabs[1]);
+            PlayerInput.Instantiate(playerPrefabs[0], pairWithDevice: Keyboard.current, controlScheme: "Keyboard&Mouse");
+            PlayerInput.Instantiate(playerPrefabs[1], pairWithDevice: Keyboard.current, controlScheme: "RightKeyboard");
         } else if (playerCount < 2) {
-            PlayerInput.Instantiate(playerPrefabs[0]);
-            PlayerInput.Instantiate(playerPrefabs[1]);
+            PlayerInput.Instantiate(playerPrefabs[0], pairWithDevice: Keyboard.current, controlScheme: "Keyboard&Mouse");
+            PlayerInput.Instantiate(playerPrefabs[1], pairWithDevice: Keyboard.current, controlScheme: "RightKeyboard");
             PlayerInput.Instantiate(playerPrefabs[2], pairWithDevice: Gamepad.all[0]);
         } else if (playerCount < 3) {
-            PlayerInput.Instantiate(playerPrefabs[0]);
-            PlayerInput.Instantiate(playerPrefabs[1]);
+            PlayerInput.Instantiate(playerPrefabs[0], pairWithDevice: Keyboard.current, controlScheme: "Keyboard&Mouse");
+            PlayerInput.Instantiate(playerPrefabs[1], pairWithDevice: Keyboard.current, controlScheme: "RightKeyboard");
             PlayerInput.Instantiate(playerPrefabs[2], pairWithDevice: Gamepad.all[0]);
             PlayerInput.Instantiate(playerPrefabs[3], pairWithDevice: Gamepad.all[1]);
         } else if (playerCount < 4) {
