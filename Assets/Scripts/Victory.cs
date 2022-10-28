5df48d07054613e3f5fe2cfe4c586d7d13ede85b
William Yang <wy2037@nyu.edu>
2022-10-28 00:55:53 -0400

    Victory Scene

--- Assets/Scripts/Victory.cs
+++ Assets/Scripts/Victory.cs
@@ -6,13 +6,15 @@ using UnityEngine.SceneManagement;
 
 public class Victory : MonoBehaviour
 {
-    //public GameObject[] playerPrefabs;
-    //public Transform spawnPoint;
+    public GameObject[] playerPrefabs;
+    public Vector2 spawnPoint;
     public TextMeshProUGUI label;
 
     void Start(){
         int winner = PlayerPrefs.GetInt("winner");
         label.text = ("Player " + winner + " wins!");
+        GameObject prefab = playerPrefabs[winner-1];
+        GameObject clone = Instantiate(prefab, spawnPoint, Quaternion.identity);
         Invoke("LoadStageSelect", 5f);
     }
 
