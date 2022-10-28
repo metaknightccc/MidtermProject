d3a191870b85cf76e83339701f00c1d3050a0e3d
metaknightccc <sunyifanc@126.com>
2022-10-28 05:33:55 -0400

    bug fix and add bgm for select page and main menu

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,18 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (FindObjectsOfType<MusicManager>().Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
     }
 }
--- Assets/Scripts/StageSelect.cs
+++ Assets/Scripts/StageSelect.cs
@@ -5,18 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class StageSelect : MonoBehaviour
 {
-    public void PokemonStadium() {
+    public void PokemonStadium()
+    {
+        SceneManager.MoveGameObjectToScene(GameObject.Find("Music"), SceneManager.GetActiveScene());
         SceneManager.LoadScene("Stage1");
     }
     public void Forest() {
+        SceneManager.MoveGameObjectToScene(GameObject.Find("Music"), SceneManager.GetActiveScene());
         SceneManager.LoadScene("Stage3");
     }
 
     public void Lava() {
+        SceneManager.MoveGameObjectToScene(GameObject.Find("Music"), SceneManager.GetActiveScene());
         SceneManager.LoadScene("Stage4");
     }
 
     public void Ice() {
+        SceneManager.MoveGameObjectToScene(GameObject.Find("Music"), SceneManager.GetActiveScene());
         SceneManager.LoadScene("Stage2");
     }
 }
